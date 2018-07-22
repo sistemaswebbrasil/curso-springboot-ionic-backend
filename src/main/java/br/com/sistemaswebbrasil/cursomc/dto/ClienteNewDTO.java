d3bff68785b01cdd3427b5da0030fcea016747c2
Adriano Faria <adriano.faria@outlook.com.br>
2018-07-22 10:49:30 -0300

    Validação customizada de CPF ou CNPJ na inserção de Cliente

--- src/main/java/br/com/sistemaswebbrasil/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/br/com/sistemaswebbrasil/cursomc/dto/ClienteNewDTO.java
@@ -2,26 +2,52 @@ package br.com.sistemaswebbrasil.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.sistemaswebbrasil.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="Email inválido")
 	private String email;
-	private String cpfOucnpj;
+
+	@NotEmpty(message="Preenchimento obrigatório")
+	private String cpfOuCnpj;
+
 	private Integer tipo;
-
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
-	private String numero;
-	private String complemento;
-	private String bairro;
-	private String cep;
 
+	@NotEmpty(message="Preenchimento obrigatório")
+	private String numero;
+
+	private String complemento;
+
+	private String bairro;
+
+	@NotEmpty(message="Preenchimento obrigatório")
+	private String cep;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String telefone1;
+
 	private String telefone2;
+	
 	private String telefone3;
 
 	private Integer cidadeId;
-
+	
 	public ClienteNewDTO() {
 	}
 
@@ -41,12 +67,12 @@ public class ClienteNewDTO implements Serializable {
 		this.email = email;
 	}
 
-	public String getCpfOucnpj() {
-		return cpfOucnpj;
+	public String getCpfOuCnpj() {
+		return cpfOuCnpj;
 	}
 
-	public void setCpfOucnpj(String cpfOucnpj) {
-		this.cpfOucnpj = cpfOucnpj;
+	public void setCpfOuCnpj(String cpfOuCnpj) {
+		this.cpfOuCnpj = cpfOuCnpj;
 	}
 
 	public Integer getTipo() {
@@ -128,5 +154,4 @@ public class ClienteNewDTO implements Serializable {
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
 	}
-
 }
