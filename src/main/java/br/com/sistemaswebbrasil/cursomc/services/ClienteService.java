d698714cb844dab0dd4a833869103d5edb8378cf
Adriano Faria <adriano.faria@outlook.com.br>
2018-07-29 22:33:47 -0300

    Restrição de conteúdo: cliente só recupera ele mesmo

--- src/main/java/br/com/sistemaswebbrasil/cursomc/services/ClienteService.java
+++ src/main/java/br/com/sistemaswebbrasil/cursomc/services/ClienteService.java
@@ -15,43 +15,52 @@ import org.springframework.transaction.annotation.Transactional;
 import br.com.sistemaswebbrasil.cursomc.domain.Cidade;
 import br.com.sistemaswebbrasil.cursomc.domain.Cliente;
 import br.com.sistemaswebbrasil.cursomc.domain.Endereco;
+import br.com.sistemaswebbrasil.cursomc.domain.enums.Perfil;
 import br.com.sistemaswebbrasil.cursomc.domain.enums.TipoCliente;
 import br.com.sistemaswebbrasil.cursomc.dto.ClienteDTO;
 import br.com.sistemaswebbrasil.cursomc.dto.ClienteNewDTO;
 import br.com.sistemaswebbrasil.cursomc.repositories.ClienteRepository;
 import br.com.sistemaswebbrasil.cursomc.repositories.EnderecoRepository;
+import br.com.sistemaswebbrasil.cursomc.security.UserSS;
+import br.com.sistemaswebbrasil.cursomc.services.exceptions.AuthorizationException;
 import br.com.sistemaswebbrasil.cursomc.services.exceptions.DataIntegrityException;
 import br.com.sistemaswebbrasil.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private BCryptPasswordEncoder pe;
 
 	@Autowired
 	private ClienteRepository repo;
-	
+
 	@Autowired
-	private EnderecoRepository enderecoRepository;	
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
+		UserSS user = UserService.authenticated();
+		
+		if (user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}		
+
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
-	
+
 	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
 		obj = repo.save(obj);
 		enderecoRepository.saveAll(obj.getEnderecos());
 		return obj;
-	}	
+	}
 
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
-		updateData(newObj,obj);
+		updateData(newObj, obj);
 		return repo.save(newObj);
 	}
 
@@ -76,25 +85,27 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null);
 	}
-	
+
 	public Cliente fromDTO(ClienteNewDTO objDto) {
-		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj() , TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));		
-		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);		
-		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
 		cli.getEnderecos().add(end);
 		cli.getTelefones().add(objDto.getTelefone1());
-		if (objDto.getTelefone2()!=null) {
+		if (objDto.getTelefone2() != null) {
 			cli.getTelefones().add(objDto.getTelefone2());
 		}
-		if (objDto.getTelefone3()!=null) {
+		if (objDto.getTelefone3() != null) {
 			cli.getTelefones().add(objDto.getTelefone3());
 		}
-		return cli;		
+		return cli;
 	}
-	
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
-		newObj.setEmail(obj.getEmail());		
-	}	
+		newObj.setEmail(obj.getEmail());
+	}
 
 }
