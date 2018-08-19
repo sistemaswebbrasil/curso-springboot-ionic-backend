80ed35afbdd0a1f3a9666e6ed399268b5087e034
Adriano Faria <adriano.faria@outlook.com.br>
2018-08-19 17:03:10 -0300

    Bonus: ajustando tamanho da imagem

--- src/main/java/com/siswbrasil/services/ClienteService.java
+++ src/main/java/com/siswbrasil/services/ClienteService.java
@@ -43,13 +43,16 @@ public class ClienteService {
 	private EnderecoRepository enderecoRepository;
 
 	@Autowired
-	private ImageService ImageService;
+	private ImageService imageService;
 
 	@Autowired
 	private S3Service s3Service;
 
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
+	
+	@Value("${img.profile.size}")
+	private Integer size;
 
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -127,10 +130,13 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado");
 		}
 
-		BufferedImage jpgImage = ImageService.getJpgImageFromFile(multipartFile);
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 
-		return s3Service.uploadFile(ImageService.getInputStream(jpgImage, "jpg"), fileName, "image");
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
 
 }
