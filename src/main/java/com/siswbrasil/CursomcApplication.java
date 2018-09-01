57ca807491d778a6af3b121e722f3f51f4fcd5f6
Adriano Faria <adriano.faria@outlook.com.br>
2018-09-01 12:23:52 -0300

    Alterando para gerar um War em vez de Jar e colocar no Tomcat

--- src/main/java/com/siswbrasil/CursomcApplication.java
+++ src/main/java/com/siswbrasil/CursomcApplication.java
@@ -3,9 +3,10 @@ package com.siswbrasil;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner {
+public class CursomcApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
